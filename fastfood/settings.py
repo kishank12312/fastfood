7cb4f8c6086c630d40ea0e3bf944b878aab01815
kishank12312 <kishankcr7@gmail.com>
2021-09-07 01:46:07 +0530

    Debug Set to True

--- fastfood/settings.py
+++ fastfood/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zavj9qq!29spd$ttdmf-e6-!c@v3vvn*qz2*x)gifbbn9$pz#q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
