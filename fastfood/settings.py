bec9d3891ef92b25921892077eb8088d723da735
kishank12312 <kishankcr7@gmail.com>
2021-10-02 17:22:21 +0530

    GIS Test

--- fastfood/settings.py
+++ fastfood/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django.contrib.gis',
     'baseapp',
     'allauth',
     'allauth.account',
